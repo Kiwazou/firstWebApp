82320645773f73539c3341b8a966b8306599d321
Kiwazou <44888754+Kiwazou@users.noreply.github.com>
2025-03-01 15:10:36 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ function addTask() {
     if (taskInput.value.trim() !== "") {
         const li = document.createElement("li");
         li.textContent = taskInput.value;
+        
+        // 削除機能を追加
+        li.addEventListener("click", function() {
+            this.remove(); // クリックすると削除
+        });
+
         taskList.appendChild(li);
         taskInput.value = "";
     }
